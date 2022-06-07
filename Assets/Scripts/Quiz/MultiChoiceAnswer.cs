e24dcc55a1de9f6051f4bff1ae315b7d3f6525fc
David Morris <pixelgrapher@outlook.co.nz>
2022-06-07 20:57:02 +1200

    Disable guess button when loading new game data

--- Assets/Scripts/Quiz/MultiChoiceAnswer.cs
+++ Assets/Scripts/Quiz/MultiChoiceAnswer.cs
@@ -26,6 +26,7 @@ public class MultiChoiceAnswer : MonoBehaviour
     {
         chosenGame = _gameData;
 
+        guessButton.interactable = false;
         SpawnAnswerCards();
         InitAnswers();
     }
