c411ff9c124a2f73fe4b624f23b70343ce9c241a
the-pixelgrapher <pixelgrapher@outlook.co.nz>
2023-11-05 12:54:45 +1300

    Add tween to progress bar widget

--- Assets/Scripts/UI/ProgressWidget.cs
+++ Assets/Scripts/UI/ProgressWidget.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
 
 public class ProgressWidget : MonoBehaviour
 {
@@ -10,11 +11,16 @@ public class ProgressWidget : MonoBehaviour
     [SerializeField] private Image progressBar;
     [SerializeField] private TMP_Text progressText;
 
+    [SerializeField] private float barTweenDuration;
+    [SerializeField] private Ease barTweenEase;
+    private Tween barTweener;
+
     public void SetProgressDisplay(int _currentRound, int _totalRounds)
     {
         _currentRound++;
         progressText.text = _currentRound.ToString() + "/" + _totalRounds.ToString();
-        progressBar.fillAmount = (float) _currentRound / _totalRounds;
+        barTweener?.Kill(true);
+        barTweener = progressBar.DOFillAmount((float)_currentRound / _totalRounds, barTweenDuration).SetEase(barTweenEase);
     }
 
     public void SetVisibility(bool _visible)
