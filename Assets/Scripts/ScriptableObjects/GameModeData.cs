fa517f0c1f0b0176b26ff02dafa285699ca1b9e5
the-pixelgrapher <pixelgrapher@outlook.co.nz>
2023-11-09 22:09:54 +1300

    Create new individual scriptable objects for game data

--- Assets/Scripts/ScriptableObjects/GameModeData.cs
+++ Assets/Scripts/ScriptableObjects/GameModeData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "GameModesData", menuName = "ScriptableObjects/GameModeData", order = 1)]
+[CreateAssetMenu(fileName = "GameModesData", menuName = "ScriptableObjects/GameModeData", order = 2)]
 public class GameModeData : ScriptableObject
 {
     [System.Serializable]
